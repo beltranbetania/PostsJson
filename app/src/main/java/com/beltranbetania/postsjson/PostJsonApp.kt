6885ff1ccc2e0672cf5d9280f85f45e27b3acee6
beltranbetania <beltranbetania@gmail.com>
2022-03-04 23:07:06 -0400

    Initial commit

---
+++ app/src/main/java/com/beltranbetania/postsjson/PostJsonApp.kt
@@ -0,0 +1,8 @@
+package com.beltranbetania.postsjson
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PostJsonApp : Application(){
+}
---
+++ app/src/main/java/com/beltranbetania/postsjson/core/Consts.kt
@@ -0,0 +1,3 @@
+package com.beltranbetania.postsjson.core
+
+const val BASE_URL: String = "https://jsonplaceholder.typicode.com/"
